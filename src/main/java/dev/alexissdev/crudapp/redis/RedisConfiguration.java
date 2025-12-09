29b013fc380a7ee2804a889d5795edd88f15c817
alexissdev <notcacha1998@gmail.com>
2025-12-09 12:29:11 -0300

    feat(product): Redis cache added for better response

--- src/main/java/dev/alexissdev/crudapp/redis/RedisConfiguration.java
+++ src/main/java/dev/alexissdev/crudapp/redis/RedisConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 public class RedisConfiguration {
 
+    public static final String PRODUCT_KEY = "product:%s";
+
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
