a4ada8b21fed0e99ce5bfbe9efe69ac16b363279
alexissdev <notcacha1998@gmail.com>
2025-12-09 12:20:54 -0300

    feat(redis): added the redis configuration

---
+++ src/main/java/dev/alexissdev/crudapp/redis/RedisConfiguration.java
@@ -0,0 +1,19 @@
+package dev.alexissdev.crudapp.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        return redisTemplate;
+    }
+
+}
